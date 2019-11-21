a575a44e8da2364380de7c2bc30678c14eb8e55b
wuwangxing <15028597033@163.com>
2019-11-21 17:33:37 +0800

    文章类别新增功能实现

--- web_back/js/utils/category.js
+++ web_back/js/utils/category.js
@@ -3,6 +3,11 @@ var cate = {
         $.get(URL.ADMIN_CATE_SEARCH, function(res) {
             callback(res)
         })
+    },
+    add: function(name, slug, callback) {
+        $.post(URL.ADMIN_CATE_ADD, { name: name, slug: slug }, function(res) {
+            callback(res)
+        })
     }
 
 
