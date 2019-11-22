f23d608bc714df1786e7b7c3d710ef3e324ed4bd
wuwangxing <15028597033@163.com>
2019-11-22 22:03:08 +0800

    发表文章

--- web_back/js/utils/article.js
+++ web_back/js/utils/article.js
@@ -8,5 +8,20 @@ var article = {
         $.get(URL.ADMIN_ARTICLE_DELETE, { id: id }, function(res) {
             callback(res)
         })
+    },
+    publish: function(obj, callback) {
+        $.ajax({
+                url: URL.ADMIN_ARTICLE_PUBLISH,
+                data: obj,
+                processData: false, // 不要让jquery去处理formData数据
+                contentType: false, // 不设置默认的请求头
+                type: 'post',
+                success: function(res) {
+                    callback(res)
+                }
+            })
+            // $.post(URL.ADMIN_ARTICLE_PUBLISH, obj, { processData: false }, { contentType: false }, function(res) {
+            //     callback(res)
+            // })
     }
 }
