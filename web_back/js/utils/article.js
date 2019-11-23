b5706608050df26f2c50324c7a8ddde781cfbb74
wuwangxing <15028597033@163.com>
2019-11-23 15:27:30 +0800

    编辑页面获取分类信息

--- web_back/js/utils/article.js
+++ web_back/js/utils/article.js
@@ -11,17 +11,15 @@ var article = {
     },
     publish: function(obj, callback) {
         $.ajax({
-                url: URL.ADMIN_ARTICLE_PUBLISH,
-                data: obj,
-                processData: false, // 不要让jquery去处理formData数据
-                contentType: false, // 不设置默认的请求头
-                type: 'post',
-                success: function(res) {
-                    callback(res)
-                }
-            })
-            // $.post(URL.ADMIN_ARTICLE_PUBLISH, obj, { processData: false }, { contentType: false }, function(res) {
-            //     callback(res)
-            // })
+            url: URL.ADMIN_ARTICLE_PUBLISH,
+            data: obj,
+            processData: false, // 不要让jquery去处理formData数据
+            contentType: false, // 不设置默认的请求头
+            type: 'post',
+            success: function(res) {
+                callback(res)
+            }
+        })
+
     }
 }
